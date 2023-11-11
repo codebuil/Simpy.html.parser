8a36a90e7029036177af76852b03268a5bd6805b
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-11-11 12:38:07 +0000

    Add files via upload

--- parser.py
+++ parser.py
@@ -9,22 +9,58 @@ s=str(page.content)
 ss=s.split("<")
 
 starts=0
+startss=0
+startsss=0
+startssss=0
 for n in ss:
     sss = n.split(">")
     mm=0
+    startss=0
     for m in sss:
-        if len(m)>=len("body"):
-            if m.find("body"):
+        m=m.strip()
+        if len(m)>=len("body") and mm==0:
+            f=m.find("body")
+            if f<2 and f>-1:
                 starts=1
-        if len(m)>=len("/body"):
-            if m.find("/body"):
+        if len(m)>=len("/body") and mm==0:
+            f=m.find("/body")
+            if f<2 and f>-1:
                 starts=0
-        if mm > 0:
-            if starts!=0:
-                print("        ",end="")
-        mm+=1
-        if starts != 0:
+        if len(m)>=len("a ") and mm==0:
+            f=m.find("a ")
+            if f<2 and f>-1:
+                startss=1
+        if len(m)>=len("/a") and mm==0:
+            f=m.find("/a")
+            if f<2 and f>-1:
+                startss=0
+        if len(m)>=len("h3") and mm==0:
+            f=m.find("h3")
+            if f<2 and f>-1:
+                startsss=1 
+             
+        if len(m)>=len("h3") and mm==0:
+            f=m.find("/h3")
+            if f<2 and f>-1:
+                startsss=0  
+        if starts != 0 and mm==0 and startss!=0 and startsss!=0:
+            m=m.replace("\\r","")
+            m=m.replace("\\n","")
+            m1:int=m.find("href=")
+            if m1>-1:
+                m=m[m1:]
+                m11=m.split("\"",m1)
+                if len(m11)>1:
+                    print("http://github.com"+m11[1].strip()+"->", end="")
+                    
+        if starts != 0 and mm>0 and startss!=0 and startsss!=0:
+            m=m.replace("\\r","")
+            m=m.replace("\\n","")
             print(m.strip())
-
-
+        if len(m)>=len("Stars") and mm==1:
+            f=m.find("Stars")
+            if f<2 and f>-1:
+                startssss=1 
+        mm+=1
+    startss=0    
 
